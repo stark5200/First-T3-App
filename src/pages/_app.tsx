3a0c0d088c9975d4620957f945a245348ff771c2
stark5200 <a.taha5200@gmail.com>
2023-06-12 03:10:05 +0200

    added clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
-//app page
+// page/_app.tsx
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import type { AppProps } from "next/app";
+ 
+
+ 
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
